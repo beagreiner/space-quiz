82be889ec2890300f48e2cb8eba8a8dafe004024
beagreiner <60270435+beagreiner@users.noreply.github.com>
2023-03-18 01:03:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ const data = {
      },
      "Uranus": {
        "card": {
-         "question": "Was ist ungewöhnlich an der Neigung von Uranus?",
+         "question": "Warum sieht Uranus türkis aus?",
          "answers": [{
            "Seine Oberfläche besteht aus Wasser": "false"
            },
@@ -132,7 +132,8 @@ const data = {
            "Auf seinen Felsen wächst Moos": "false"
            }
          ],
-         "successMessage": "Die blaue Farbe bei Neptun und Uranus kommt von dem Methan in der Atmosphäre der beiden Planeten. Methan verschluckt den roten Anteil des Lichts, so dass der Planet blau erscheint.": "Your next target is at...."
+         "successMessage": "Die blaue Farbe bei Neptun und Uranus kommt von dem Methan in der Atmosphäre der beiden Planeten. Methan verschluckt den roten Anteil des Lichts, so dass der Planet blau erscheint.",
+         "nextTargetHint": "Your next target is at...."
        }
      },
      "Neptun": {
